73dcf4fb1ddd637d90bcc412dada058d570adf6e
Usha Rani <pannuri.usha-rani@capgemini.com>
2026-06-17 15:20:12 +0530

    Add 01_CreateElement

---
+++ 01_CreateElement/source.js
@@ -0,0 +1,12 @@
+const heading = React.createElement("div", { id: "root" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "heading1"),
+    React.createElement("h1", {}, "heading2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "heading1"),
+    React.createElement("h1", {}, "heading2"),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.querySelector("#main"));
+root.render(heading);
